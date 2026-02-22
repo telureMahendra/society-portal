f79b381854454d3c176acbb4ddbcbb0f2ced0f62
Mahendra Telure <mahendra.telure@vizpay.in>
2026-02-23 04:26:52 +0530

    config updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://localhost:8080/api/v1',
+    apiBaseUrl: 'http://llptech.cloud:8080/api/v1',
     defaultSubdomain: 'lodha' // Default for development
 };
+
