1494be5be4b585655bb3e1405e849705b5732438
Mahendra Telure <mahendra.telure@vizpay.in>
2026-05-30 17:23:48 +0530

    Base URL updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
-export const environment = {
-    production: false,
-    apiBaseUrl: 'http://localhost:8080/api/v1',
-    defaultSubdomain: 'lodha' // Default for development
-};
-
-
 // export const environment = {
 //     production: false,
-//     apiBaseUrl: 'http://194.164.149.201:8080/api/v1',
+//     apiBaseUrl: 'http://localhost:8080/api/v1',
 //     defaultSubdomain: 'lodha' // Default for development
 // };
+
+
+export const environment = {
+    production: false,
+    apiBaseUrl: 'http://194.164.149.201:8080/api/v1',
+    defaultSubdomain: 'lodha' // Default for development
+};
 
 
